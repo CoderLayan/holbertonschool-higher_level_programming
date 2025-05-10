7dd7b1f385340cad0526092f27f53c6de6b37b2c
layan <layanazez@gmail.com>
2025-05-10 14:55:43 +0000

    Add 3-print_alphabt.py: prints alphabet without q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ascii_code in range(97, 123):
-    if ascii_code not in [101, 113]:
-        print(chr(ascii_code), end="")
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print("{:c}".format(letter), end="")
