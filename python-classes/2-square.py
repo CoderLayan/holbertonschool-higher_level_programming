41f97ac27df120bef856f88d4fb0ef632592ebe3
Your Name <layanazez@gmail.com>
2025-05-23 15:19:48 +0000

    Added Rectangle class with eval-compatible representation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Defines a square with private size attribute."""
+""" Define a class Square """
+
 
 class Square:
-    """Class that defines a square with private size attribute."""
+    """Represent a square."""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
