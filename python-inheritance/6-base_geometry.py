bef36bfb0ff72692827f27b4c3360a24ca382d2b
layan <layanazez@gmail.com>
2025-05-28 10:22:41 +0000

    Task 6: Improve BaseGeometry with area() method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing improved BaseGeometry class with area method."""
+
+
+class BaseGeometry:
+    """Base geometry class with area method."""
+
+    def area(self):
+        """Calculate the area (not implemented).
+        
+        Raises:
+            Exception: Always raises with message 'area() is not implemented'
+        """
+        raise Exception("area() is not implemented")
