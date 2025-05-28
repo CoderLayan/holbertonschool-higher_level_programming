0add1c3e13720f1e0ccaf0c279cc9f55e9da4614
layan <layanazez@gmail.com>
2025-05-28 11:03:09 +0000

    Fixed PEP8 validation and added integer validation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry"""
+"""Defines a base geometry class BaseGeometry."""
+
 
 class BaseGeometry:
-    """this class represents a base geometry"""
+    """Represents a base geometry."""
 
     def area(self):
-        """method not implemented yet"""
+        """Raise exception indicating area() is not implemented.
+        Raises:
+            Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates a value as an integer
+        """Validate a value as an integer.
+        Args:
+            name (str): The name of the parameter
+            value (int): The value to validate
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is <= 0
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
