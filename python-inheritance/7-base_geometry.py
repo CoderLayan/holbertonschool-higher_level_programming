9719d15c6e7a1692905f2f90b0c5ee923ba96b5c
layan <layanazez@gmail.com>
2025-05-28 10:44:30 +0000

    Fixed PEP8 type comparison error (E721)

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,26 +1,11 @@
-#!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry"""
+def area(self):
+    """method not implemented yet"""
+    raise Exception("area() is not implemented")
 
-
-class BaseGeometry:
-    """This class represents a base geometry."""
-
-    def area(self):
-        """Raises an exception indicating the method is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates that value is a positive integer.
-
-        Args:
-            name (str): The name of the variable.
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+def integer_validator(self, name, value):
+    """validates a value as an integer
+    """
+    if type(value) != int:
+        raise TypeError("{} must be an integer".format(name))
+    if value <= 0:
+        raise ValueError("{} must be greater than 0".format(name))
