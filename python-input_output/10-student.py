3afd14d5e71a4eefd81460b21026e8518c5ca674
layan <layanazez@gmail.com>
2025-06-02 10:25:50 +0000

    Fix PEP8 style in student JSON filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -12,13 +12,15 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance
+        """Retrieves a dictionary representation of a Student instance
         If attrs is a list of strings, only includes those attributes
         """
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) 
-                    for attr in attrs if hasattr(self, attr)}
+        if (isinstance(attrs, list) and 
+                all(isinstance(attr, str) for attr in attrs)):
+            return {
+                attr: getattr(self, attr) 
+                for attr in attrs if hasattr(self, attr)
+            }
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
