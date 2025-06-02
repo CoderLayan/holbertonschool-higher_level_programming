5a307cdf678c4576a1e10d62029b9ad27dac510c
layan <layanazez@gmail.com>
2025-06-02 10:26:52 +0000

    Fix remaining PEP8 whitespace issues

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -15,10 +15,10 @@ class Student:
         """Retrieves a dictionary representation of a Student instance
         If attrs is a list of strings, only includes those attributes
         """
-        if (isinstance(attrs, list) and 
-                all(isinstance(attr, str) for attr in attrs)):
+        if (isinstance(attrs, list) and \
+                all(isinstance(attr, str) for attr in attrs):
             return {
-                attr: getattr(self, attr) 
+                attr: getattr(self, attr)
                 for attr in attrs if hasattr(self, attr)
             }
         return {
