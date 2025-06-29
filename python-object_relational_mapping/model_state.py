cbdbb1857085ef1bb64f97df9a8c3b675317278a
layan <layanazez@gmail.com>
2025-06-29 08:46:18 +0000

    Task 6: State model definition

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,14 +8,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     Represents a state in the MySQL table 'states'
+    
     Attributes:
         id (int): Auto-generated, unique identifier (primary key)
         name (str): Name of the state (max 128 chars, not null)
     """
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
